8a4b336fed0a1684c102b2ff276c140ba0714049
Welew <will@willbelew.com>
2023-11-29 08:51:42 -0800

    working on UnionFind for accountsMerge, no pure solve

--- accountsMerge.py
+++ accountsMerge.py
@@ -1,7 +1,63 @@
 import collections
 
 
+class UnionFind():
+    # from : https://www.scaler.com/topics/data-structures/disjoint-set/
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [0 for i in range(n)]
+
+    def find(self, node):
+        # returns root of node
+        if node == self.parent[node]:
+            return node
+        else:
+            # path compression --> bringing the searched node closer to parent for faster subsequent lookups
+            self.parent[node] = self.find(self.parent[node])
+            return self.parent[node]
+
+    def union(self, a, b):
+        # determine higher rank node, then use that as parent
+        a_leader = self.find(a)
+        b_leader = self.find(b)
+        if a_leader != b_leader:
+            if self.rank[a_leader] < self.rank[b_leader]:
+                # swapping elements if rank of A-root is LESS than rank of B-root (otherwise, no swap needed)
+                temp = a_leader
+                a_leader = b_leader
+                b_leader = temp
+            # THE KEY step --> actually hanging b-dj to a_leader
+            self.parent[b_leader] = a_leader
+            # incrementing rank if needed (ie, only if ranks are equal)
+            if self.rank[a_leader] == self.rank[b_leader]:
+                self.rank[a_leader] += 1
+
+
 def accountsMerge(accounts):
+    uf = UnionFind(len(accounts))
+    emails = {}
+    for idx, a in enumerate(accounts):
+        for e in a[1:]:
+            if e in emails:
+                uf.union(idx, emails[e])
+            else:
+                emails[e] = idx
+
+    emailGroup = collections.defaultdict(list)
+    for e, i in emails.items():
+        # find SINGLE account to act as leader
+        leader = uf.find(i)
+        emailGroup[leader].append(e)
+
+    res = []
+    for idx in emailGroup:
+        merged_acc = [accounts[idx][0]] + sorted(emailGroup[idx])
+        res.append(merged_acc)
+
+    return res
+
+
+def accountsMergeWB(accounts):
     emails = collections.defaultdict(list)
     res = []
     for idx, a in enumerate(accounts):
@@ -43,4 +99,5 @@ test_accounts_1 = [["John", "johnsmith@mail.com", "john_newyork@mail.com"], ["Jo
                                                                              "johnsmith@mail.com", "john00@mail.com"], ["Mary", "mary@mail.com"], ["John", "johnnybravo@mail.com"]]
 test_accounts_2 = [["Gabe", "Gabe0@m.co", "Gabe3@m.co", "Gabe1@m.co"], ["Kevin", "Kevin3@m.co", "Kevin5@m.co", "Kevin0@m.co"], ["Ethan", "Ethan5@m.co",
                                                                                                                                 "Ethan4@m.co", "Ethan0@m.co"], ["Hanzo", "Hanzo3@m.co", "Hanzo1@m.co", "Hanzo0@m.co"], ["Fern", "Fern5@m.co", "Fern1@m.co", "Fern0@m.co"]]
-print(accountsMerge(test_accounts_2))
+print(accountsMerge(test_accounts_1))
+# print(accountsMergeWB(test_accounts_2))
