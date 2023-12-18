e96bd44d6077519ca3975e58697625d3dcf8e497
Welew <will@willbelew.com>
2023-12-18 09:47:54 -0800

    chipping away at CA algo implementation

---
+++  accountsMergeDFS.py
@@ -0,0 +1,9 @@
+[["John", "johnsmith@mail.com", "john_newyork@mail.com"],
+ ["John", "johnsmith@mail.com", "john00@mail.com"],
+ ["Mary", "mary@mail.com"],
+ ["John", "johnnybravo@mail.com"]]
+
+{"johnsmith@mail.com": {"johnsmith@mail.com", "johnsmith@mail.com", "john00@mail.com"},
+ "john00@mail.com": {"johnsmith@mail.com"},
+ "mary@mail.com": {"mary@mail.com"},
+ "johnnybravo@mail.com": {"johnnybravo@mail.com"}}
--- CA_background/hexaCA.js
+++ CA_background/hexaCA.js
@@ -8,20 +8,24 @@ canvas.height = window.innerHeight;
 function draw() {}
 
 let grid = [];
-console.log(grid);
+//console.log(grid);
 
-function initGrid(mouse_pos, sq_dimension = 30) {
+function initGrid(mouse_pos, sq_dimension = 25) {
   let grid_width = Math.floor(canvas.width / sq_dimension);
   let grid_height = Math.floor(canvas.height / sq_dimension);
   let mouse_grid_x = Math.floor(mouse_pos[0] / sq_dimension);
   let mouse_grid_y = Math.floor(mouse_pos[1] / sq_dimension);
   //console.log(mouse_grid_x, mouse_grid_y);
+  //console.log(grid_width, grid_height);
+  grid = [];
 
   ctx.strokeStyle = "red";
   ctx.fillStyle = "red";
   ctx.strokeWidth = 2;
   for (let i of Array(grid_height).keys()) {
+    let row = [];
     for (let j of Array(grid_width).keys()) {
+      let cell = "";
       if (mouse_grid_x === j && mouse_grid_y === i) {
         ctx.fillRect(
           j * sq_dimension,
@@ -29,6 +33,7 @@ function initGrid(mouse_pos, sq_dimension = 30) {
           sq_dimension,
           sq_dimension
         );
+        cell = "origin";
       } else {
         ctx.strokeRect(
           j * sq_dimension,
@@ -37,7 +42,9 @@ function initGrid(mouse_pos, sq_dimension = 30) {
           sq_dimension
         );
       }
+      row.push(cell);
     }
+    grid.push(row);
   }
 }
 function getCursorPosition(canvas, event) {
@@ -47,13 +54,28 @@ function getCursorPosition(canvas, event) {
   //console.log(x, y);
   return [x, y];
 }
+
+let timer = null;
+let animationInterval = null;
+
 canvas.addEventListener("mousemove", function (event) {
+  if (animationInterval) {
+    clearInterval(animationInterval);
+  }
+  if (timer) {
+    clearTimeout(timer);
+  }
   const cursorPosition = getCursorPosition(canvas, event);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   // Convert cursor position to hexagonal grid position and set the cell to "alive"
   //console.log(cursorPosition);
-  [x, y] = cursorPosition;
+
   initGrid(cursorPosition);
+  /* after mousemove has cleaned/updated the background, kick off CA algo  */
+  //console.log(grid);
+  timer = setTimeout(() => {
+    triggerAnimation(cursorPosition);
+  }, 1000);
   //drawHexagon();
 });
 window.addEventListener("resize", () => {
@@ -61,26 +83,77 @@ window.addEventListener("resize", () => {
   canvas.height = window.innerHeight;
   initGrid([0, 0]);
 });
-function getHexagonNeighbors(x, y) {
+
+canvas.addEventListener("mouseout", () => {
+  console.log("leaving canvas: ", animationInterval);
+  if (animationInterval) {
+    clearInterval(animationInterval);
+    animationInterval = null;
+  }
+});
+canvas.addEventListener("mouseenter", () => {
+  console.log("entering canvas at: ", animationInterval);
+  if (animationInterval) {
+    clearInterval(animationInterval);
+    animationInterval = null;
+  }
+});
+
+function getSquareNeighbors(x, y) {
   // Based on x and y, return the neighbors for a hexagon cell
+  let neighbors = new Array(8).fill(null);
+  let deltas = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+  ];
+  for (let n = 0; n < neighbors.length; n++) {
+    let n_delta = deltas.at(n);
+    let y_delta = n_delta.at(1);
+    let x_delta = n_delta.at(0);
+    if (
+      y + y_delta >= 0 &&
+      y + y_delta < grid.length &&
+      x + x_delta >= 0 &&
+      x + x_delta < grid.at(0).length
+    ) {
+      neighbors[n] = [x + x_delta, y + y_delta];
+    }
+  }
+  return neighbors;
 }
 
 function updateGrid() {
   // Implement your cellular automata rules and update the hexagonal grid
+  /* iterate thru `grid`
+  if item is origin, add neighbors to 'to_a';
+  if item is "a", check neighbors, add neighbors to 'to_a' if empty or to 'to_d' if "a", add item to 'to_d' 
+  if item is "d", continue
+  update grid w/ to_- vals (first a's, then d's)?
+  color accordingly
+  */
+  let to_a = [];
+  let to_d = [];
+  for (let i of grid.keys()) {
+    for (let j of grid.at(i).keys()) {
+      if (grid.at(i).at(j) === "origin") {
+        neighbors = getSquareNeighbors(j, i);
+      }
+    }
+  }
 }
 
-function drawHexagon(x, y) {
-  // Use the canvas 2D API to draw a polygon at (x, y) with the specified size
-}
+function triggerAnimation(cursorPosition) {
+  if (animationInterval) clearInterval(animationInterval);
 
-function render() {
-  // Draw the hexagonal grid on the canvas based on cell states
-}
-
-function animate() {
-  requestAnimationFrame(animate);
-  updateGrid();
-  render();
+  animationInterval = setInterval(() => {
+    updateGrid();
+  }, 1000);
 }
 
 initGrid([0, 0]);
